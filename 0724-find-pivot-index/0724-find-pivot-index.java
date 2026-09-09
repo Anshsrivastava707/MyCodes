c878356ed472424155ce9094cf723bd58390dc3d
Anshsrivastava707 <ansh.srivastava520@gmail.com>
2026-09-09 15:11:44 +0530

    Time: 13 ms (9.5%), Space: 44.4 MB (99.95%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -3,10 +3,11 @@ class Solution {
         //if(nums.length==0 || nums.length==1) return nums.length-1;
         int [] prefix=new int[nums.length];
         prefix[0]=nums[0];
+        System.gc();
         for(int i=1;i<nums.length;i++){
             prefix[i]=prefix[i-1]+nums[i];
         }
-    System.gc();
+    
         for(int i=nums.length-2;i>=0;i--){
             nums[i]=nums[i]+nums[i+1];
         }
