1075507d556dba721b0644bf3aed435826fd7413
Anshsrivastava707 <ansh.srivastava520@gmail.com>
2026-09-25 07:46:56 +0530

    Time: 1 ms (49.83%), Space: 43.1 MB (34.04%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,44 +1,54 @@
+// class Solution {
+//     public int strStr(String haystack, String needle) {
+//         if (needle.isEmpty()) {
+//             return 0;
+//         }
+
+//         int lps[] = new int[needle.length()];
+//         int start = 0;
+//         int end = 1;
+        
+//         while (end < needle.length()) { 
+//             if (needle.charAt(end) == needle.charAt(start)) {
+//                 start++;
+//                 lps[end] = start;
+//                 end++;
+//             } else if (start != 0) {
+//                 start = lps[start - 1];
+//             } else {
+//                 lps[end] = 0;
+//                 end++;
+//             }
+//         }
+
+//         int Hidx = 0;
+//         int Nidx = 0;
+        
+//         while (Hidx < haystack.length()) {
+//             if (haystack.charAt(Hidx) == needle.charAt(Nidx)) { 
+//                 Hidx++;
+//                 Nidx++;
+//             } else if (Nidx != 0) { 
+//                 Nidx = lps[Nidx - 1]; 
+//             } else {
+//                 Hidx++; 
+//             }
+
+//             if (Nidx == needle.length()) {
+//                 return Hidx - needle.length();
+//             }
+//         }
+        
+//         return -1; 
+//     }
+// }
 class Solution {
     public int strStr(String haystack, String needle) {
-        if (needle.isEmpty()) {
-            return 0;
-        }
-
-        int lps[] = new int[needle.length()];
-        int start = 0;
-        int end = 1;
-        
-        while (end < needle.length()) { 
-            if (needle.charAt(end) == needle.charAt(start)) {
-                start++;
-                lps[end] = start;
-                end++;
-            } else if (start != 0) {
-                start = lps[start - 1];
-            } else {
-                lps[end] = 0;
-                end++;
+        for(int i=0;i<=haystack.length()-needle.length();i++){
+            if((haystack.substring(i,i+needle.length())).equals(needle)){
+                return i;
             }
         }
-
-        int Hidx = 0;
-        int Nidx = 0;
-        
-        while (Hidx < haystack.length()) {
-            if (haystack.charAt(Hidx) == needle.charAt(Nidx)) { 
-                Hidx++;
-                Nidx++;
-            } else if (Nidx != 0) { 
-                Nidx = lps[Nidx - 1]; 
-            } else {
-                Hidx++; 
-            }
-
-            if (Nidx == needle.length()) {
-                return Hidx - needle.length();
-            }
-        }
-        
-        return -1; 
+        return -1;
     }
 }
